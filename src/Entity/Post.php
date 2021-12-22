a3bd5ab4970869626f7d46b39b9ad5ed24ed29cc
Takashi Kanemoto <ttskch@gmail.com>
2021-12-22 18:59:53 +0900

    feat: 🎸 PostエンティティをAPIリソースとして設定

--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -2,11 +2,13 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\PostRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity(repositoryClass=PostRepository::class)
+ * @ApiResource()
  */
 class Post
 {
